92a75f7ae2bcecb36aa99c14cd2d021b0ee801e7
Ivan Petkov <petkow.iv@gmail.com>
2022-02-20 23:09:02 +0200

    Add Hook for DB and util for db connection

---
+++ projects/AutomationForHomeworkTasks/AutomationForHomeworkTasks/Steps/Hooks/DBHook.cs
@@ -0,0 +1,26 @@
+﻿using AutomationForHomeworkTasks.Utils;
+using System;
+using TechTalk.SpecFlow;
+
+namespace AutomationForHomeworkTasks.Steps.Hooks
+{
+    [Binding]
+    public class DBHook
+    {
+        [BeforeTestRun]
+        public static void InitDBConnection()
+        {
+            Console.WriteLine("Initializing the DB connection...");
+            DBConnection.Init();
+            Console.WriteLine("Successfully initialized DB connection!");
+        }
+
+        [AfterTestRun]
+        public static void CloseDBConnection()
+        {
+            Console.WriteLine("Closing the DB connection...");
+            DBConnection.Close();
+            Console.WriteLine("Successfully closed DB connection!");
+        }
+    }
+}
---
+++ projects/AutomationForHomeworkTasks/AutomationForHomeworkTasks/Utils/DBConnection.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using MySql.Data.MySqlClient;
+
+namespace AutomationForHomeworkTasks.Utils
+{
+    public class DBConnection
+    {
+        private readonly MySqlConnection _connection;
+        private static DBConnection _dbConnection;
+        private const string CONNECTION_STRING = "Server=localhost;Port=3306;Database=db;Uid=root;Pwd=pass;";
+
+        private DBConnection()
+        {
+            _connection = new MySqlConnection(CONNECTION_STRING);
+            try
+            {
+                Console.WriteLine("Connecting to DB...");
+                _connection.Open();
+                Console.WriteLine("Successfully connected to DB");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Problem with connection to DB");
+                Console.WriteLine(e.Message);
+            }
+        }
+           
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public static DBConnection Init()
+        {
+            if (_dbConnection is null)
+            {
+                _dbConnection = new DBConnection();
+            }
+            return _dbConnection;
+        }
+
+        public static void InsertRecord(string sqlQuery)
+        {
+            MySqlCommand command = new(sqlQuery, _dbConnection._connection);
+            MySqlDataAdapter adapter = new();
+            adapter.InsertCommand = command;
+
+            Console.WriteLine($"Inserting into DB with SQL: {sqlQuery}");
+            if (adapter.InsertCommand.ExecuteNonQuery() < 1)
+            {
+                Console.WriteLine("Nothing has been inserted...");
+            }
+            command.Dispose();
+        }
+
+        public static void Close()
+        {
+            _dbConnection._connection.Dispose();
+            _dbConnection = null;
+        }
+    }
+}
